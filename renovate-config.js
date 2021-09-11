a4c17ba87beb49bb6d3999640f19288355767226
Anne Stellingwerf <astellin@opentext.com>
2021-09-11 13:45:46 +0200

    First attempt at setting up a Github action for a project

---
+++ renovate-config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "$schema": "https://docs.renovatebot.com/renovate-schema.json",
+  "platform": "github",
+  "username": "renovate@labelvier.nl",
+  "gitAuthor": "Renovate Bot <renovate@labelvier.nl>",
+  "baseDir": "/tmp/renovate",
+  "extends": ["config:base"],
+  "dryRun": true
+}
